902715ba6af7feac76cc63d51ad8e672b5c846c6
Your Name <you@example.com>
2015-09-17 20:50:19 +1200

    Implementada pulga nueva de Gabi.

--- Fleattack/Unity/New Unity Project/Assets/Jump.cs
+++ Fleattack/Unity/New Unity Project/Assets/Jump.cs
@@ -8,10 +8,10 @@ public class Jump : MonoBehaviour {
 	private float impulseX = Random.Range(-3f, 3f);
 	private float impulseY = Random.Range(5f, 10f);
 
-	private float torqueX = Random.Range(-200f, 200f);
-	private float torqueY = Random.Range(-200f, 200f);
-
 	void Start () {
+		float torqueX = Random.Range(-200f, 200f);
+		float torqueY = Random.Range(-200f, 200f);
+	
 		rb = GetComponent<Rigidbody> ();
 		rb.AddForce (new Vector3 (impulseX, impulseY), ForceMode.Impulse);
 		rb.AddTorque (torqueX, torqueY, 0);
