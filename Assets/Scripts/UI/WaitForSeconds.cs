9da9fce37a2231aa574d4faa1195ac44c6603157
Pedro Worcel <pedro@worcel.com>
2015-11-01 01:16:53 +1300

    add scenes

---
+++ Assets/Scripts/UI/WaitForSeconds.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitForSeconds : MonoBehaviour {
+
+	public float delay;
+
+	void Start() {
+		Invoke ("LoadMenu", 4);
+	}
+
+	void LoadMenu() {
+		Application.LoadLevel("Menu");
+	}
+}
