07963c14f51bad21579c1a4244ddd29904d03351
gabrielamelian <gabrielamelian@gmail.com>
2017-02-21 22:45:57 +1300

    Fix bug with highscore not resetting, refactor score manager

--- Assets/Scripts/HighScore/HighScoreController.cs
+++ Assets/Scripts/HighScore/HighScoreController.cs
@@ -1,31 +1,22 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
-using System.IO;
 using System;
 
 public class HighScoreController : MonoBehaviour {
 
     public Text nameTextObject;
     public Text scoreTextObject;
-    public static string STORE_FILENAME = "/HighScores.txt";
 
     private string scoreFilePath;
-    private int MAX_SCORES = 5;
 
     void Start() {
-        this.scoreFilePath = Application.persistentDataPath + STORE_FILENAME;
-        if(!ApplicationData.lastScore.saved) {
-            WriteHighScore(ApplicationData.lastScore);
-            ApplicationData.lastScore.saved = true;
-        }
-
         DisplayScores();
     }
 
     void DisplayScores() {
-        List<Score> scores = GetCurrentScores();
+        List<Score> scores = ScoreManager.GetCurrentScores();
 
         string nameString = "";
         string scoreString = "";
@@ -38,101 +29,9 @@ public class HighScoreController : MonoBehaviour {
         scoreTextObject.text = scoreString;
     }
 
-    void WriteHighScore(Score scoreToWrite) {
-        bool alreadyAppended = false;
-        List<Score> scores = GetCurrentScores();
-        List<Score> newScores = new List<Score>();
-        foreach(Score s in scores) {
-            if(newScores.Count == MAX_SCORES) {
-                break;
-            }
-
-            if(scoreToWrite.score >= s.score && !alreadyAppended) {
-                newScores.Add(scoreToWrite);
-                newScores.Add(s);
-                alreadyAppended = true;
-            } else {
-                newScores.Add(s);
-            }
-        }
-
-        WriteHighScoresToFile(newScores);
-    }
-
-    void WriteHighScoresToFile(List<Score> scores) {
-        int i = 0;
-        string scoresStr = "";
-        int count = scores.Count;
-        foreach(Score s in scores) {
-            scoresStr += s.name + ",";
-            scoresStr += s.score;
-
-            if(i < count - 1) {
-                scoresStr += System.Environment.NewLine;
-            }
-
-            i += 1;
-        }
-
-        File.WriteAllText(scoreFilePath, scoresStr);
-    }
 
     public void BackToMenu() {
         SceneManager.LoadScene("Menu");
-    }
-
-    public static List<Score> GetCurrentScores() {
-        string scoreFilePath = GetFilePath();
-        bool fileExists = File.Exists(scoreFilePath);
-        if(fileExists) {
-            return GetScoresFromFile();
-        } else {
-            return GetFakeScores();
-        }
-    }
-
-    static List<Score> GetScoresFromFile() {
-        List<Score> scores = new List<Score>();
-        string[] lines = File.ReadAllLines(GetFilePath());
-        foreach(string line in lines) {
-            string[] splat = line.Split(',');
-
-            Score score = new Score();
-            score.saved = true;
-            score.name = splat[0];
-            score.score = Int32.Parse(splat[1]);
-
-            scores.Add(score);
-        }
-
-        return scores;
-    }
-
-    public static List<Score> GetFakeScores() {
-        List<Score> scores = new List<Score>();
-
-        scores.Add(new Score { name = "Wiki", score = 1000 });
-        scores.Add(new Score { name = "Gaby", score = 750 });
-        scores.Add(new Score { name = "Altu", score = 500 });
-        scores.Add(new Score { name = "Peter", score = 137 });
-        scores.Add(new Score { name = "Eze", score = 133 });
-
-        return scores;
-    }
-
-    public static string GetFilePath() {
-        return Application.persistentDataPath + HighScoreController.STORE_FILENAME;
-    }
-
-    public static bool IsNewHighScore(int newScore) {
-        List<Score> scores = GetCurrentScores();
-        foreach(Score s in scores) {
-            if(newScore >= s.score) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
 }
