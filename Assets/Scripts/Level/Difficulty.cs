d0b73243ba75465df17a195d62de6b2e263a7b2c
gabrielamelian <gabrielamelian@gmail.com>
2017-02-25 16:31:37 +1300

    Adds comments to flea spawner, create a new empty class to control difficulty

---
+++ Assets/Scripts/Level/Difficulty.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Difficulty : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
