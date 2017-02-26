49703411cf91e861f8cee16f67e00c5e6ef0cdaf
gabrielamelian <gabrielamelian@gmail.com>
2017-02-26 21:28:33 +1300

    Adds new submit button on enter new highscore

--- Assets/Scripts/Level/Difficulty.cs
+++ Assets/Scripts/Level/Difficulty.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Difficulty : MonoBehaviour {
+public class Difficulty {
+	
+	/// <summary>
+	/// Initial values for variables.
+	/// </summary>
+	public int initialMaxFleas = 1;
+	public float initialPercentageSpawn = 60f;
+	public float initialTimeBetweenSpawns = 0.8f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	/// <summary>
+	/// Time between difficulty changes.
+	/// </summary>
+	public float timeBetweenDifficulty = 10f;
+
+	/// <summary>
+	/// Upper limits of individual difficulty increases.
+	/// </summary>
+	public float maxPercentageSpawn = 80f;
+	public float minTimeBetweenSpawns = 0.2f;
+
 }
+
